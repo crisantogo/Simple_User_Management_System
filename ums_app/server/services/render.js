6c62c0693fc82ff5a6506b7cdc51bb1f25d700ec
crisantogo <christopher.gopeesingh@gmail.com>
2022-07-21 17:50:45 -0400

    updated routes

--- ums_app/server/services/render.js
+++ ums_app/server/services/render.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 exports.homeRoutes = (req,res)=>{
     //Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get('http://https://rocky-atoll-58236.herokuapp.com/')
     .then(function(response){
         res.render('index', {users:response.data})        
     })
@@ -16,7 +16,7 @@ exports.add_user = (req,res)=>{
 }
 
 exports.update_user = (req,res)=>{
-    axios.get('http://localhost:3000/api/users',{params:{id:req.query.id}})
+    axios.get('http://https://rocky-atoll-58236.herokuapp.com/',{params:{id:req.query.id}})
     .then(function(userdata){
         res.render("update_user",{user:userdata.data})
     })
